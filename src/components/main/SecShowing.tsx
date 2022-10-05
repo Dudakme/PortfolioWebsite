35eed99f4be7112a58b6ba524caa85c300632426
D <61399248+BotGUyz@users.noreply.github.com>
2022-10-05 18:49:23 +0700

    adding some experiences

--- src/components/main/SecShowing.tsx
+++ src/components/main/SecShowing.tsx
@@ -25,12 +25,33 @@ const SecSec: React.FC<any> = () => {
         />
         <div className="max-w-max"></div>
       </div>
-      <div className="inline-flex justify-center">
+      <div className="inline-flex justify-center w-screen mb-10">
         <Card
           title="Android"
           description="Kotlin 으로 안드로이드 앱 제작 경험. (코틀린 너무 조아 ^ㅇ^)"
         />
         <Image src="/android.svg" width="248" height="248" />
+      </div>
+      <div className="inline-flex justify-center w-screen mb-10">
+        <Image src="/nestjs.svg" width="248" height="248" />
+        <Card
+          title="Nest.js"
+          description="Nest.js 를 이용한 REST API 제작 경험. (간단 간편 너무 좋다 뿡)"
+        />
+      </div>
+      <div className="inline-flex justify-center w-screen mb-10">
+        <Card
+          title="Ktor"
+          description="Ktor 를 이용한 REST API 제작 경험. (간단 간편 그리고 완벽한 파일 구조 :o )"
+        />
+        <Image src="/ktor.svg" width="248" height="248" />
+      </div>
+      <div className="inline-flex justify-center w-screen mb-10">
+      <Image src="/react.svg" width="248" height="248" />
+        <Card
+          title="React"
+          description="React 를 이용한 스태딕 애플리케이션. (Next.js 전엔 리액트! >_<)"
+        />
       </div>
     </main>
   );
