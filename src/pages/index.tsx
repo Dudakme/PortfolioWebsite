daa41870e965de0c0d8893c521346d3a33046785
D <61399248+BotGUyz@users.noreply.github.com>
2022-10-17 21:43:23 +0700

    fix fillOpacity
    pls

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home() {
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path
           fill="#2563EB"
-          fill-opacity="1"
+          fillOpacity="1"
           d="M0,320L120,304C240,288,480,256,720,213.3C960,171,1200,117,1320,90.7L1440,64L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"
         ></path>
       </svg>
