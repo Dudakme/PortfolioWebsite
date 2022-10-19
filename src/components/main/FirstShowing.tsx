10ad6e4cb21ab3a1d458615cd95f79d0177d222e
D <61399248+BotGUyz@users.noreply.github.com>
2022-10-19 19:19:41 +0700

    Updates on design

--- src/components/main/FirstShowing.tsx
+++ src/components/main/FirstShowing.tsx
@@ -4,24 +4,26 @@ import WaveAnimation from "./waveAnimation";
 
 const FirstSec: React.FC<any> = () => {
   return (
-    <div className="flex flex-col mt-40 relative">
+    <div className="h-screen">
+      <div className="flex flex-col mt-40 relative">
       <WaveAnimation />
       <div>
-        <main className="pt-4 text-center pb-72 bg-blue-600">
+        <main className="pt-4 text-center pb-72 bg-blue-600 h-screen">
           <Image
             src="/fish.svg"
             width="100"
             height="100"
-            className="mt-20 animate-pulse"
+            className="mt-20"
           />
-          <h1 className="font-bold drop-shadow-sm text-5xl z-10 mb-3 text-white">
+          <h1 className="font-bold drop-shadow-sm text-5xl z-10 mb-3 text-gray-700">
             학생 개발자
           </h1>
-          <p className="text-xl text-gray-300 drop-shadow-sm">
+          <p className="text-xl text-gray-600 drop-shadow-sm">
             안녕하세요. 개발을 좋아하는 평범한 학생입니다.
           </p>
         </main>
       </div>
+    </div>
     </div>
   );
 };
